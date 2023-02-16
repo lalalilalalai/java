971103dc9e93ea7783b7e0ff5c6e84cccacfa99d
lalalilalalai <116339275+lalalilalalai@users.noreply.github.com>
2023-02-16 14:17:54 +0700

    updated task find substring in string

--- substring-in-string/src/main/java/Main.java
+++ substring-in-string/src/main/java/Main.java
@@ -2,18 +2,23 @@ import java.util.Locale;
 
 public class Main {
 
+
     public static void main(String[] args) {
-        System.out.println(countLetterInString("Fairytale", "a"));
-        System.out.println(countLetterInString("Fairytale", "t"));
-        System.out.println(countLetterInString("Fairytale", "f"));
-        System.out.println(countLetterInString("Fairytale", "k"));
+        System.out.println(countLetterInString("Fairytale", 'a'));
+        System.out.println(countLetterInString("Fairytale", 't'));
+        System.out.println(countLetterInString("Fairytale", 'f'));
+        System.out.println(countLetterInString("Fairytale", 'T'));
+        System.out.println(countLetterInString("Fairytale", 'k'));
+        System.out.println(countLetterInString("Сказка", 'з'));
     }
 
-    public static int countLetterInString(String input, String letter) {
+    public static int countLetterInString(String input, char letter) {
+//        TODO Method arguments are String and char. Count how many times a char appears in a string.
+//        TODO Входные параметры - строка и символ. Посчитать, сколько раз символ встречается в строке.
         input = input.toLowerCase(Locale.ROOT);
-        letter = letter.toLowerCase(Locale.ROOT);
+        letter = Character.toLowerCase(letter);
         int count = 0;
-        while (input.contains(letter)) {
+        while (input.indexOf(letter) != -1) {
             count++;
             int i = input.indexOf(letter);
             input = input.substring(i + 1);

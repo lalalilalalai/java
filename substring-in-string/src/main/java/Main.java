e7370e727a721bc15fe3cf0be37a9bc2e0259bc9
lalalilalalai <116339275+lalalilalalai@users.noreply.github.com>
2023-02-17 15:44:22 +0700

    updated task substring-in-string

--- substring-in-string/src/main/java/Main.java
+++ substring-in-string/src/main/java/Main.java
@@ -4,19 +4,20 @@ public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println(countLetterInString("Fairytale", 'a'));
-        System.out.println(countLetterInString("Fairytale", 't'));
-        System.out.println(countLetterInString("Fairytale", 'f'));
-        System.out.println(countLetterInString("Fairytale", 'T'));
-        System.out.println(countLetterInString("Fairytale", 'k'));
-        System.out.println(countLetterInString("Сказка", 'з'));
+        System.out.println(countLetterInString("Fairytale", 'f', true));
+        System.out.println(countLetterInString("Fairytale", 'f', false));
+        System.out.println(countLetterInString("Fairytale", 'T', false));
+        System.out.println(countLetterInString("Fairytale", 'k', false));
+        System.out.println(countLetterInString("Сказка", 'З', false));
     }
 
-    public static int countLetterInString(String input, char letter) {
+    public static int countLetterInString(String input, char letter, boolean isCaseSensitive) {
 //        TODO Method arguments are String and char. Count how many times a char appears in a string.
 //        TODO Входные параметры - строка и символ. Посчитать, сколько раз символ встречается в строке.
-        input = input.toLowerCase(Locale.ROOT);
-        letter = Character.toLowerCase(letter);
+        if (!isCaseSensitive) {
+            input = input.toLowerCase(Locale.ROOT);
+            letter = Character.toLowerCase(letter);
+        }
         int count = 0;
         while (input.indexOf(letter) != -1) {
             count++;

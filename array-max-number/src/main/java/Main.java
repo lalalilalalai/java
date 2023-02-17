0b451f0c8295847bb397037753d9747fb73ef301
lalalilalalai <116339275+lalalilalalai@users.noreply.github.com>
2023-02-17 12:04:19 +0700

    added task array-max-number

--- array-max-number/src/main/java/Main.java
+++ array-max-number/src/main/java/Main.java
@@ -1,4 +1,19 @@
 public class Main {
 
-//    TODO Задача 3: Необходимо написать метод, который во входящем массиве вернет максимальное число из массива. (Реализация должна быть через цикл)
+    public static void main(String[] args) {
+        int[] nums = {7, 11, 37, 0, -19, 37, -38}; // max = 37
+        System.out.println(getArrayMaxNum(nums));
+    }
+
+    public static int getArrayMaxNum(int[] nums) {
+        //    TODO Method which returns array max num (loop implementation is necessary)
+        //    TODO Метод, возвращающий макс значение из входного массива (реализация через цикл)
+        int max = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > max) {
+                max = nums[i];
+            }
+        }
+        return max;
+    }
 }
